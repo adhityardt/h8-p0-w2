a5899de25a81c8be281bd563807c085ed552a1b8
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-02-02 17:12:26 +0700

    Telah menyelesaikan exercise-9.js

---
+++ exercise-9.js
@@ -0,0 +1,17 @@
+//TUGAS 1
+function shoutOut(){
+  console.log("Halo Function!");
+}
+console.log(shoutOut());
+
+//TUGAS 2
+function calculateMultiply(number1, number2) {
+  return hasilPerkalian = number1 * number2;
+}
+console.log(calculateMultiply(5, 6));
+
+//TUGAS 3
+function processSentence (name, age, adress, hobby){
+  console.log(`Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${adress}, dan saya punya hobby yaitu ${hobby}`);
+}
+console.log(processSentence('Agus', 30, 'Jln. Malioboro, Yogyakarta', 'gaming'));
