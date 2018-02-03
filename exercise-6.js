379ede72ad3d7665721293277ba52cb796a5251d
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-02-03 16:26:27 +0700

    telah menyelesaikan exercise-6.js

--- exercise-6.js
+++ exercise-6.js
@@ -14,5 +14,43 @@ while (counter >= 0) {
   console.log(counter + ' - I will become fullstack developer');
   counter -= increment;
 }
+
 //2. Melakukan Looping Menggunakan For
-for()
+console.log('LOOPING PERTAMA');
+var limit = 20;
+for(var counter = 1; counter <= limit; counter++){
+  console.log(counter + ' - I love coding');
+}
+console.log('LOOPING KEDUA');
+counter--;
+for(limit = 1; limit <= counter; counter--){
+  console.log(counter + ' - I will become fullstack developer');
+}
+
+//3. Angka Ganjil dan Genap
+console.log('Menampilkan GANJIL dan GENAP untuk nilai counter');
+let limit = 100;
+for(let counter = 1; counter <= limit; counter++){
+  if(counter%2 === 1){
+    console.log('GANJIL');} else {
+      console.log('GENAP');
+    }
+}
+console.log('Menampilkan 3 KELIPATAN 3 pada counter');
+for(let counter = 1; counter <= limit; counter += 2){
+  if(counter%3 === 0){
+    console.log('3 KELIPATAN 3');
+  } else{console.log(' ');}
+}
+console.log('Menampilkan 6 KELIPATAN 6 pada counter');
+for(let counter = 1; counter <= limit; counter += 5){
+  if(counter%6 === 0){
+    console.log('6 KELIPATAN 6');
+  } else{console.log(' ');}
+}
+console.log('Menampilkan 10 KELIPATAN 10 pada counter');
+for(let counter = 1; counter <= limit; counter += 9){
+  if(counter%10 === 0){
+    console.log('10 KELIPATAN 10');
+  } else{console.log(' ');}
+}
