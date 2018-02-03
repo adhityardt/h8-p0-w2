016c86f60e76042374bfbf9a8d6dd06199f60ebe
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-02-03 18:51:57 +0700

    telah menyelesaikan exercise-12.js

---
+++ exercise-12.js
@@ -0,0 +1,20 @@
+//Konversi Menit
+function konversiMenit(menit) {
+  let jam = menit / 60;
+  let displayJam = Math.floor(jam);
+  let displayMenit1 = jam - displayJam;
+  let displayMenit2 = displayMenit1 * 60;
+  displayMenit2 = Math.floor(displayMenit2);
+  if(menit > 60 && displayMenit2 < 10){
+    console.log(`${displayJam}:0${displayMenit2}`);
+  } else if (menit > 60 && displayMenit2 >= 10){
+    console.log(`${displayJam}:${displayMenit2}`);
+  } else if (menit < 60 && displayMenit2 >= 10) {
+    console.log(`00:${displayMenit2}`);
+  } else {
+    console.log(`00:0${displayMenit2}`);
+  }
+  return;
+}
+konversiMenit(100);
+//output 1:40
