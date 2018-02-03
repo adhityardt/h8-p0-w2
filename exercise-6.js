6766617bda5615a3923669aebc91b51821142fb5
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-02-03 11:16:49 +0700

    Just finished the first one, still two to go on exercise-6.js

---
+++ exercise-6.js
@@ -0,0 +1,17 @@
+//1. Melakukan Looping Menggunakan while
+console.log('LOOPING PERTAMA');
+var counter = 2;
+var limit = 20;
+var increment = 2;
+while (counter <= limit){
+  console.log(counter + ' - I love coding');
+  counter += increment;
+}
+
+console.log('LOOPING KEDUA');
+counter -= 2;
+while (counter >= 0) {
+  console.log(counter + ' - I will become fullstack developer');
+  counter -= increment;
+}
+//2. Melakukan Looping Menggunakan For
