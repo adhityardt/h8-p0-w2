4d6652fa01cc61588207882ebc79904b531aa591
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-02-04 20:30:53 +0700

    finished exercise-3.js

---
+++ exercise-3.js
@@ -0,0 +1,22 @@
+let nama = "Mikael";
+let peran = "";
+
+if( nama === "" && peran === "") {
+    console.log("Nama harus diisi!");
+}
+
+if( nama === "Mikael") {
+  console.log("Halo " +nama+", pilih peranmu untuk memulai game!");
+}
+
+if( nama === "Nina" && peran === "Ksatria") {
+    console.log("Selamat datang di dunia Proxytia "+nama+". Halo "+peran+" "+nama+", kamu dapat menyerang dengan senjatamu!");
+}
+
+if(nama === "Danu" && peran === "Tabib") {
+    console.log("Selamat datang di dunia Proxytia, "+nama+". Halo "+peran+" "+nama+", kamu akan membantu temanmu yang terluka.");
+}
+
+if( nama === "Zero" && peran === "Penyihir") {
+    console.log("Selamat datang di dunia Proxytia, "+nama+". Halo "+peran+" "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
+}
